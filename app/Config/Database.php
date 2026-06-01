7844602a717c5d7904dfb792924dd27c675e1d0e
proyectodegrado2026svp-hash <proyectodegrado2026svp@gmail.com>
2026-06-01 16:08:14 -0400

    Update Database.php

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -41,7 +41,7 @@ class Database extends Config
         'compress'     => false,
         'strictOn'     => false,
         'failover'     => [],
-        'port'         => 5432,
+        'port'         => 6543,
         'numberNative' => false,
         'foundRows'    => false,
         'dateFormat'   => [
